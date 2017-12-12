3c1423e3017ef04941cead983bcf53649c25f315
Alexander Razzhivin <admin@httpbots.com>
2017-12-12 17:32:55 +0300

    Рисуем точки

---
+++ public/2-points/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+  background(255);
+  strokeWeight(10);
+  point(200, 200);
+  point(180, 180);
+  point(220, 220);
+  point(180, 220);
+  point(220, 180);
+}
